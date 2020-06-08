ead1a2e319a6fba7e6121456d49f5f6904ab2814
Panny Lewis <pannylewis@gmail.com>
2020-06-07 23:17:43 -0500

    Form.js:  added inputs, added props, set states to match with name of input, export and imported Form.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Form from "./Forms";
 
 const membersArray = [
   {
@@ -8,40 +9,48 @@ const membersArray = [
     name: "Panny",
     github: "https://github.com/PannyLewis",
     email: "panny-lewis@lambdastudents.com",
+    role: "Frontend",
   },
   {
     id: "2",
     name: "Shomari",
     github: "https://github.com/slroberts",
     email: "shomari-roberts@lambdastudents.com",
+    role: "Frontend",
   },
   {
     id: "3",
     name: "Sara",
     github: "https://github.com/reidysj",
     email: "sara-reidy@lambdastudents.com",
+    role: "Project Manager",
   },
 ];
 
 function App() {
-  const [members, setMembers] = useState([]);
+  const [members, setMembers] = useState([membersArray]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+
+    <div>
+      <div className="form-container">
+        <Form />
+      </div>
 
       <div className="members-container">
         {membersArray.map((item) => (
@@ -49,10 +58,13 @@ function App() {
             <p>{item.name}</p>
             <p>{item.github}</p>
             <p>{item.email}</p>
+            <p>{item.role}</p>
           </div>
         ))}
       </div>
     </div>
+
+    // </div>
   );
 }
 
