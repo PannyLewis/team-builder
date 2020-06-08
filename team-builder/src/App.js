8753aa5f9e729aa2d0b0773adc7f126acf5a6046
Panny Lewis <pannylewis@gmail.com>
2020-06-07 21:36:11 -0500

    App.js:  created an array and tried to map throught the array, but not rendering.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const membersArray = [
+  {
+    id: "0",
+    name: "Panny",
+    github: "https://github.com/PannyLewis",
+    email: "panny-lewis@lambdastudents.com",
+  },
+  {
+    id: "2",
+    name: "Shomari",
+    github: "https://github.com/slroberts",
+    email: "shomari-roberts@lambdastudents.com",
+  },
+  {
+    id: "3",
+    name: "Sara",
+    github: "https://github.com/reidysj",
+    email: "sara-reidy@lambdastudents.com",
+  },
+];
 
 function App() {
+  const [members, setMembers] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +42,16 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div className="members-container">
+        {membersArray.map((item) => (
+          <div className="member-card" key={item.id}>
+            <p>{item.name}</p>
+            <p>{item.github}</p>
+            <p>{item.email}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
